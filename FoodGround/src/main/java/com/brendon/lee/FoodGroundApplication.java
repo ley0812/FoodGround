0c4ef864bfce5e6e1b053c67f19a1a2580abc0d8
ley0812 <ley0812@gmail.com>
2016-11-04 17:17:52 +0900

    First Commit

---
+++ FoodGround/src/main/java/com/brendon/lee/FoodGroundApplication.java
@@ -0,0 +1,12 @@
+package com.brendon.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodGroundApplication.class, args);
+	}
+}
---
+++ FoodGround/src/test/java/com/brendon/lee/FoodGroundApplicationTests.java
@@ -0,0 +1,18 @@
+package com.brendon.lee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FoodGroundApplication.class)
+@WebAppConfiguration
+public class FoodGroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
